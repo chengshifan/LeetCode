6c957879f300e4d58b153b0e8372b93cccb23a05
chengshifan <545591082@qq.com>
2020-08-24 21:49:01 +0800

    20200824

---
+++ 152-maximum-product-subarray/Solution.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        num_max, num_min, res = nums[0], nums[0], nums[0]
+        for i in range(1, len(nums)):
+            pre_max = max(num_max * nums[i], num_min * nums[i], nums[i])
+            pre_min = min(num_max * nums[i], num_min * nums[i], nums[i])
+            res = max(pre_max, res)
+            num_max = pre_max
+            num_min = pre_min
+        return res
+
+
+print(Solution().maxProduct([-4, -1, -2]))
