795cdc7fd5df92b8ec6b1365e5201e406dddacfc
chengshifan <545591082@qq.com>
2020-08-27 20:36:34 +0800

    20200827

---
+++ 162-find-peak-element/Solution.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        if not nums:
+            return None
+        n = len(nums)
+        if n == 1:
+            return 0
+        if nums[1] < nums[0]:
+            return 0
+        if nums[n - 2] < nums[n - 1]:
+            return n - 1
+
+        def helper(nums, start, end) -> int:
+            mid = (start + end) // 2
+            left, right = mid - 1, mid + 1
+            if left >= 0 and right <= n - 1:
+                if nums[left] < nums[mid] > nums[right]:
+                    return mid
+                elif nums[left] > nums[mid] > nums[right]:
+                    return helper(nums, 0, mid)
+                else:
+                    return helper(nums, mid, end)
+
+        return helper(nums, 0, n)
+
+
+print(Solution().findPeakElement([1, 2, 1, 3, 5, 6, 4]))
