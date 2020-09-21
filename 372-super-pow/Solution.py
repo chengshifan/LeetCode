66525ad54597627a3c7e29914c8e89ddafeafcaa
chengshifan <545591082@qq.com>
2020-09-21 20:50:46 +0800

    20200921

---
+++ 372-super-pow/Solution.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def superPow(self, a: int, b: List[int]) -> int:
+        if not b or len(b) == 0:
+            return 1
+        tmp = b[-1]
+
+        def helper(num: int, c: int):
+            if c == 0:
+                return 1
+            num %= 1337
+            res = 1
+            for i in range(c):
+                res *= num
+                res %= 1337
+            return res
+
+        part_1 = helper(a, tmp)
+        part_2 = helper(self.superPow(a, b[0:-1]), 10)
+        return (part_1 * part_2) % 1337
+
+
+print(Solution().superPow(2, [1, 1]))
