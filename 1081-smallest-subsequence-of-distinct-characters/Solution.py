0c6c8d9c9a4b963970f06c9b3b97bf64095bdfee
chengshifan <545591082@qq.com>
2020-11-09 20:48:23 +0800

    20201109

---
+++ 1081-smallest-subsequence-of-distinct-characters/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        stack = []
+        for i in range(len(s)):
+            if s[i] not in stack:
+                while stack and stack[-1] > s[i] and stack[-1] in s[i + 1:]:
+                    stack.pop()
+                stack.append(s[i])
+        return "".join(stack)
